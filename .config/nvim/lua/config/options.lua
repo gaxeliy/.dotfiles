cd7b976f8c2ebf236083ec27c7e1ae738dc865e5
Алексей Гладких <gaxeliy@gmail.com>
2025-08-04 11:19:34 +0500

    update configs

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -11,3 +11,40 @@ vim.g.lazyvim_python_ruff = "ruff"
 vim.opt.tabstop = 8
 vim.opt.shiftwidth = 8
 vim.opt.softtabstop = 8
+
+vim.diagnostic.config({
+  virtual_text = {
+    spacing = 2,
+    severity_limit = "Warning",
+    format = function(diagnostic)
+      return diagnostic.message:gsub("\n.*", "") -- только первая строка
+    end,
+  },
+  virtual_lines = false,
+})
+
+local ns = vim.api.nvim_create_namespace("ExpandedDiagnostics")
+
+vim.api.nvim_create_autocmd("CursorHold", {
+  callback = function()
+    local bufnr = vim.api.nvim_get_current_buf()
+    local cursor = vim.api.nvim_win_get_cursor(0)
+    local lnum = cursor[1] - 1
+
+    -- Получаем диагностики только для текущей строки
+    local diagnostics = vim.diagnostic.get(bufnr, { lnum = lnum })
+
+    -- Показываем их с virtual_lines
+    vim.diagnostic.show(ns, bufnr, diagnostics, {
+      virtual_lines = true,
+      virtual_text = false,
+    })
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "CursorMoved", "InsertEnter", "BufLeave" }, {
+  callback = function()
+    local bufnr = vim.api.nvim_get_current_buf()
+    vim.diagnostic.hide(ns, bufnr)
+  end,
+})
